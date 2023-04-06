05583b85f4ff25576a427e2c51c856b4c3fdf6e1
Nie Shiyi <shiyi.nie@qitantech.com>
2023-04-06 20:45:54 +0800

    fix: 添加history.block函数，并在他的回调函数中返回true，发现无法正常跳转页面

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,17 +1,18 @@
 import { useEffect } from 'react'
 import Guide from '@/components/Guide';
 import { trim } from '@/utils/format';
-import { PageContainer } from '@ant-design/pro-components';
 import { useModel, history } from '@umijs/max';
 import styles from './index.less';
 
 const HomePage = () => {
   const { name } = useModel('global');
 
+  // history.block的回调函数不管有没有返回值或是返回任何值，都会阻止路由跳转。按理说teturn true应该是允许正常跳转的，但是现在还是被阻止。
   useEffect(() => {
-    // 如果在这个
     const unblock = history.block(({ location }) => {
       console.log('地址被拦截', location)
+      // return false
+      // return "hello"
       return true;
     });
     return () => {
